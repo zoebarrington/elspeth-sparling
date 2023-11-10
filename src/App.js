c9cebc80619afd4f6dedc762d437b6cdb1a23b39
Zoe Barrington <zoe.barrington@infinityworks.com>
2023-11-10 17:45:22 +0000

    adding cms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import './App.css';
-// import Artworks from './Artworks.js';
-// import Nav from './Nav.js';
-// import Contact from './pages/Contact.js';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import About from './pages/About.js';
+import CMS from 'netlify-cms-app';
+import 'netlify-cms-app/dist/cms.css'; 
 
 function App() {
+  CMS.init({
+    config: {
+      backend: {
+        name: 'github',
+        repo: 'your-username/your-repo',
+      },
+      media_folder: 'static/uploads',
+      public_folder: '/uploads',
+      collections: [
+        {
+          name: 'posts',
+          label: 'Posts',
+          folder: '_posts',
+          create: true,
+          fields: [
+            { label: 'Title', name: 'title', widget: 'string' },
+            { label: 'Body', name: 'body', widget: 'markdown' },
+          ],
+        },
+      ],
+    },
+  });
   return (
     <div className="App">
       <header className="App-header">ELSPETH SPARLING</header>
-      {/* <Nav></Nav>
-      <Router>
-        <div>
-      <Routes>
-      <Route path="/" element={<Artworks />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/artworks" element={<Artworks />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-    </div>
-    </Router> */}
 
     </div>
   );
